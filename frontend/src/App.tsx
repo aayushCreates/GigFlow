9ab04631f91a02c9dc7894fddd127ad07156513a
AyushAJS <jangidayush89@gmail.com>
2026-01-15 07:41:07 +0530

    frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login.";
+import Register from "./pages/Register";
+import GigsFeed from "./pages/GigFeed";
+import GigDetails from "./pages/GigDetails";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<GigsFeed />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/gig/:gigId" element={<GigDetails />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Toaster } from "sonner";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./context/auth.context";
+
+createRoot(document.getElementById("root")!).render(
+    <BrowserRouter>
+      <Toaster position="top-right" />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+);
